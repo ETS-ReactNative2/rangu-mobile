a4e7c368b86f9b4c764bff66d55a76b598a219e2
Leonardo Mariotto <leonardo_mariotto@yahoo.com.br>
2021-10-12 21:41:38 -0300

    Layout da tela de configurações pronto

--- src/screens/Settings.js
+++ src/screens/Settings.js
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, KeyboardAvoidingView, SafeAreaView, Image, TextInput, TouchableOpacity, ImageBackground, Animated, Platform, } from "react-native";
-import { FontAwesome, MaterialIcons, MaterialCommunityIcons,Feather } from '@expo/vector-icons';
+import { FontAwesome, MaterialIcons, MaterialCommunityIcons, Feather, AntDesign } from '@expo/vector-icons';
+import Hoshi from '../inputTexts/Hoshi';
 
 import img1 from '../../assets/images/Profile/FotoPerfil2_Cortada.jpg';
 const user =
 {
     key: String(Math.random()),
     personProfileImg: img1,
-    personName: 'Leonardo',
+    personName: 'Leonardo Mariotto',
+    personEmail: 'leonardo_mariotto@yahoo.com.br',
+    personPhone: '+55 (19)981824269'
 }
 
 export default function HomeScreen({ navigation }) {
@@ -15,6 +18,12 @@ export default function HomeScreen({ navigation }) {
     return (
 
         <SafeAreaView style={[styles.background]} >
+            <View style={[styles.containerBack]}>
+                <TouchableOpacity>
+                    <AntDesign name="left" size={45} color="white" />
+                </TouchableOpacity>
+            </View>
+
             <View style={styles.header}>
                 <View>
                     <TouchableOpacity>
@@ -32,22 +41,37 @@ export default function HomeScreen({ navigation }) {
             </View>
 
             <View style={styles.body}>
-                <View style={styles.celula}>
-                    <Text>Nome: Leonardo</Text>
-                </View>
-                <View style={styles.celula}>
-                    <Text>Nome: Leonardo</Text>
-                </View>
-                <View style={styles.celula}>
-                    <Text>Nome: Leonardo</Text>
-                </View>
-                <View style={styles.celula}>
-                    <Text>Nome: Leonardo</Text>
-                </View>
-                <View style={styles.celula}>
-                    <Text>Nome: Leonardo</Text>
+                <View style={styles.celulaContainer}>
+                    <View style={styles.celula}>
+                        <Text style={styles.text}>Nome:</Text>
+                        <TouchableOpacity style={styles.editBtn}>
+                            <Text style={styles.text}>{user.personName}</Text>
+                            <AntDesign name="right" size={20} color="#535357" />
+                        </TouchableOpacity>
+                    </View>
+                    <View style={styles.celula}>
+                    <Text style={styles.text}>E-Mail:</Text>
+                        <TouchableOpacity style={styles.editBtn}>
+                            <Text style={styles.text}>{user.personEmail}</Text>
+                            <AntDesign name="right" size={20} color="#535357" />
+                        </TouchableOpacity>
+                    </View>
+                    <View style={styles.celula}>
+                    <Text style={styles.text}>Telefone:</Text>
+                        <TouchableOpacity style={styles.editBtn}>
+                            <Text style={styles.text}>{user.personPhone}</Text>
+                            <AntDesign name="right" size={20} color="#535357" />
+                        </TouchableOpacity>
+                    </View>
+                    <View style={styles.celula}>
+                        <Text>Nome: Leonardo</Text>
+                    </View>
+                    <View style={styles.celula}>
+                        <Text>Nome: Leonardo</Text>
+                    </View>
                 </View>
             </View>
+
 
 
         </SafeAreaView>
@@ -60,6 +84,12 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: 'rgba(0, 0, 0, 1)',
         alignItems: 'center',
+    },
+    containerBack: {
+        position: 'absolute',
+        top: "7.1%",
+        left: "1.5%",
+        zIndex: 1,
     },
     header: {
         height: 55,
@@ -84,31 +114,51 @@ const styles = StyleSheet.create({
         height: 140,
         width: 140,
     },
-    iconContainer:{
+    iconContainer: {
         backgroundColor: '#fff',
-        borderRadius:100,
+        borderRadius: 100,
         position: 'absolute',
         alignItems: 'center',
-        justifyContent:'center',
-        width:"27%",
+        justifyContent: 'center',
+        width: "27%",
         height: "25%",
         alignSelf: 'flex-end',
         bottom: "5%",
     },
     body:
     {
-        marginTop:50,
+        marginTop: 50,
         backgroundColor: "#1e222b",
-        borderRadius:30,
+        borderRadius: 20,
         width: "90%",
+        flexDirection: 'column',
+        alignContent: 'center',
+        justifyContent: 'flex-end',
+
+    },
+    celulaContainer:
+    {
+        marginVertical: 10
     },
     celula:
     {
+        flexDirection: 'row',
         height: 40,
-        justifyContent:'center',
-        marginHorizontal:20,
-        marginBottom:10,
-        borderBottomWidth:1,
-        borderColor:'#fff',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginLeft: 20,
+        marginBottom: 15,
+        borderBottomWidth: 0.5,
+        borderColor: '#3e3d40',
+    },
+    text:
+    {
+        fontSize: 15,
+        color: '#fff',
+    },
+    editBtn:
+    {
+        flexDirection: 'row',
+        marginRight:10,
     }
 });
